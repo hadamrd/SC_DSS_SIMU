ff4578936dcfe1fcd1ca9b761cc342d3fc5cfcab
Sanaa Tiss <sanaa.tiss@mines-albi.fr>
2021-11-27 22:31:59 +0100

    refacto and history modiffs

--- history.py
+++ history.py
@@ -1,6 +1,7 @@
 import openpyxl
 import json 
 import os
+import re 
 
 def sumOverAffiliates(quantity, affiliates, products, horizon):
     return {
@@ -16,33 +17,56 @@ def generate(history_folder, results_folder, template_file):
     affiliates = data["affiliates"]
     horizon = data["horizon"]
     
-    wb = openpyxl.load_workbook(template_file)
-
-    product_sales_history = []
-    prod_plan_history = []
-    supply_demand_history = []
-    supply_plan_history = []
-    prod_demand_history = []
-
+    start_week = None
+    end_week = None
+    for file_name in os.listdir(history_folder):
+        if file_name.startswith("snapshot_S"):
+            week = int(re.match(".*S(\d+).*", file_name).group(1))
+            start_week = min(week, start_week) if start_week is not None else week
+            end_week = max(week, end_week) if end_week is not None else week
+    nbr_weeks = end_week - start_week + 1
+    
+    product_sales_history = [None] * nbr_weeks
+    prod_plan_history =  [None] * nbr_weeks
+    supply_demand_history =  [None] * nbr_weeks
+    supply_plan_history =  [None] * nbr_weeks
+    prod_demand_history =  [None] * nbr_weeks
     for file_name in os.listdir(history_folder):
         if file_name.startswith("snapshot_S"):
             with open(os.path.join(history_folder, file_name)) as fp:
                 snapshot = json.load(fp)
-            supply_demand_history.append(sumOverAffiliates(snapshot["supply_demand"], affiliates, products, horizon))
-            prod_plan_history.append(snapshot["prod_plan"])
-            supply_plan_history.append(sumOverAffiliates(snapshot["supply_plan"], affiliates, products, horizon))
-            prod_demand_history.append(snapshot["prod_demand"])
-            product_sales_history.append(sumOverAffiliates(snapshot["sales_forcast"], affiliates, products, horizon))
+            week = int(re.match(".*S(\d+).*", file_name).group(1))
+            supply_demand_history[week-start_week] = snapshot["supply_demand"]
+            prod_plan_history[week-start_week] = snapshot["prod_plan"]
+            supply_plan_history[week-start_week] = snapshot["supply_plan"]
+            prod_demand_history[week-start_week] = snapshot["prod_demand"]
+            product_sales_history[week-start_week] = snapshot["sales_forcast"]
 
-    nbr_weeks = len(prod_plan_history)
+    if not os.path.exists(results_folder):
+        os.mkdir(results_folder)
+
+    wb = openpyxl.load_workbook(template_file)
     for p in products:
         for w in range(nbr_weeks):
             for t in range(horizon):
-                wb["PV"].cell(row=3+w, column=3+t+w).value = product_sales_history[w][p][t]
-                wb["BA"].cell(row=3+w, column=3+t+w).value = supply_demand_history[w][p][t]
+                wb["PV"].cell(row=3+w, column=3+t+w).value = sum([product_sales_history[w][a][p][t] for a in affiliates if p in data["affiliate_product"][a]])
+                wb["BA"].cell(row=3+w, column=3+t+w).value = sum([supply_demand_history[w][a][p][t] for a in affiliates if p in data["affiliate_product"][a]])
                 wb["BP"].cell(row=3+w, column=3+t+w).value = prod_demand_history[w][p][t]
                 wb["PDP"].cell(row=3+w, column=3+t+w).value = prod_plan_history[w][p][t]
-                wb["PA"].cell(row=3+w, column=3+t+w).value = supply_plan_history[w][p][t]
-        if not os.path.exists(results_folder):
-            os.mkdir(results_folder)
+                wb["PA"].cell(row=3+w, column=3+t+w).value = sum([supply_plan_history[w][a][p][t] for a in affiliates if p in data["affiliate_product"][a]])
         wb.save(os.path.join(results_folder, f"{p}_results.xlsx"))
+
+    wb = openpyxl.load_workbook(template_file)
+    wb.remove_sheet(wb["PDP"])
+    wb.remove_sheet(wb["BP"])
+    for a in affiliates:
+        for p in data["affiliate_product"][a]:
+            for w in range(nbr_weeks):
+                for t in range(horizon):
+                     wb["PV"].cell(row=3+w, column=3+t+w).value = product_sales_history[w][a][p][t]
+                     wb["BA"].cell(row=3+w, column=3+t+w).value = supply_demand_history[w][a][p][t]
+                     wb["PA"].cell(row=3+w, column=3+t+w).value = supply_plan_history[w][a][p][t]
+            wb.save(os.path.join(results_folder, f"{a}_{p}_results.xlsx"))
+
+
+
--- simulation.py
+++ simulation.py
@@ -5,16 +5,22 @@ from model.model import Model
 from model.smooth_filter import SmoothFilter
 from model.risk_manager import RiskManager
 
+def replicateFile(src, dst):
+    with open(src) as fp:
+        src_d = json.load(fp)
+    with open(dst, "w") as fp:
+        json.dump(src_d, fp)
 
 def simuWithoutPlatform(start_week, end_week, inputs_folder, history_folder):
     model = Model(f"simu_inputs/global_input.json")
+    if not os.path.exists(history_folder):
+        os.mkdir(history_folder)
+    if not os.path.exists(inputs_folder):
+        os.mkdir(inputs_folder)
+    replicateFile("simu_inputs/input_S2.json", os.path.join(inputs_folder, "input_S2.json"))
     for k in range(start_week, end_week+1):
         model.loadWeekInput(os.path.join(inputs_folder, f"input_S{k}.json"))
-        model.runAffiliatesToCDC()
-        model.runCDCToFactory()
-        model.runCDCToAffiliates()
-        if not os.path.exists(history_folder):
-            os.mkdir(history_folder)
+        model.runWeek()
         model.saveSnapShot(os.path.join(history_folder, f"snapshot_S{k}.json"))
         model.generateNextWeekInput(os.path.join(inputs_folder, f"input_S{k+1}.json"))
     
@@ -36,14 +42,12 @@ def simuWithPlatform(start_week, end_week):
         model.saveSnapShot(f"simu_history/snapshot_S{k}.json")
         model.generateNextWeekInput(f"simu_inputs/input_S{k+1}.json")
 
-def simuWithAutomatedStrat(start_week, end_week):
-    sales_forcast_folder = "inputs_without_plateforme"
-    history_folder = "with_strat_history"
-    inputs_folder = "with_strat_inputs"
-    
+def simuWithAutomatedStrat(start_week, end_week, sales_forcast_folder, inputs_folder, history_folder):
+    if not os.path.exists(history_folder):
+        os.mkdir(history_folder)
     if not os.path.exists(inputs_folder):
         os.mkdir(inputs_folder)
-    
+    replicateFile("simu_inputs/input_S2.json", os.path.join(inputs_folder, "input_S2.json"))
     model = Model(f"simu_inputs/global_input.json")
     n = model.horizon - 4
     risk_manager = RiskManager(n)
@@ -55,38 +59,43 @@ def simuWithAutomatedStrat(start_week, end_week):
         with open(f"{sales_forcast_folder}/input_S{k}.json") as fp:
             old_input = json.load(fp)
         model.sales_forcast = old_input["sales_forcast"]
-        model.runAffiliatesToCDC()
-        model.runCDCToFactory()
-        model.runCDCToAffiliates()
+        model.runWeek()
         model.pa_cdc.supply_plan = filter.run(risk_manager, model)
-        if not os.path.exists(history_folder):
-            os.mkdir(history_folder)
         model.saveSnapShot(f"{history_folder}/snapshot_S{k}.json")
         model.generateNextWeekInput(f"{inputs_folder}/input_S{k+1}.json")
 
 if __name__ == "__main__":
-    history_folder = "history_without_plateforme"
-    inputs_folder= "inputs_without_plateforme"
     start_week = 2
     end_week = 40
 
-    # simuWithoutPlatform(start_week, end_week, inputs_folder, history_folder)
-    
-    # history.generate(history_folder="simu_history",
-    #                 results_folder="simu_excel_results",
-    #                 template_file="templates/template_simu_result.xlsx")
+    # simuWithPlatform(start_week, end_week)    
 
-    # simuWithPlatform(start_week, end_week)
+    # simuWithoutPlatform(
+    #     start_week, 
+    #     end_week, 
+    #     inputs_folder="inputs_without_plateforme",
+    #     history_folder="history_without_plateforme"
+    # )
+    history.generate(
+        history_folder="history_without_plateforme",
+        results_folder="without_plateforme_excel_results",
+        template_file="templates/template_simu_result.xlsx"
+    )     
 
-    # simuWithAutomatedStrat(start_week, end_week)
+    # simuWithAutomatedStrat(
+    #     start_week,
+    #     end_week,     
+    #     sales_forcast_folder="inputs_without_plateforme",
+    #     inputs_folder = "inputs_with_strat",
+    #     history_folder="history_with_strat"
+    # )
+    history.generate(
+        history_folder="history_with_strat",
+        results_folder="with_strat_excel_results",
+        template_file="templates/template_simu_result.xlsx"
+    )
 
-    history.generate(history_folder="with_strat_history",
-                results_folder="with_strat_excel_results",
-                template_file="templates/template_simu_result.xlsx")
 
-    # history.generate(history_folder="history_without_plateforme",
-    #             results_folder="without_plateforme_excel_results",
-    #             template_file="templates/template_simu_result.xlsx")
 
     
 
